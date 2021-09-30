18e2a8f4248413d88e9fc7932fa9b3d3cb4b0c0f
k906506 <k906506@naver.com>
2021-09-30 10:56:41 +0900

    Review 쿼리문 추가

---
+++ app/src/main/java/com/example/bookstore/dao/ReviewDao.kt
@@ -0,0 +1,16 @@
+package com.example.bookstore.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.bookstore.model.Review
+
+@Dao
+interface ReviewDao {
+    @Query("SELECT * FROM review WHERE id == :id")
+    fun getOneReview(id : Int) : Review
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun saveReview(review : Review)
+}
